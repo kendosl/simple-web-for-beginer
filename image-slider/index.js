12d02a29c8d4ccaa7fdbe3df804d651c0ef30d7a
kendosl <116136261+kendosl@users.noreply.github.com>
2022-10-19 14:20:40 +0700

    Add files via upload

---
+++ image-slider/index.js
@@ -0,0 +1,36 @@
+const nextEl = document.querySelector(".next");
+const prevEl = document.querySelector(".prev");
+const imageContainer = document.querySelector(".image-container");
+const imagesEl = document.querySelectorAll("img");
+
+let currentImage = 1;
+
+let timeout;
+
+nextEl.addEventListener("click", () => {
+    currentImage++;
+    clearTimeout(timeout);
+    updateImage();
+});
+
+prevEl.addEventListener("click", () => {
+    currentImage--;
+    clearTimeout(timeout);
+    updateImage();
+});
+
+updateImage();
+
+function updateImage() {
+    if (currentImage > imagesEl.length) {
+        currentImage = 1;
+    } else if (currentImage < 1) {
+        currentImage = imagesEl.length;
+    }
+    imageContainer.style.transform = `translateX(-${(currentImage - 1) * 500}px)`;
+
+    timeout = setTimeout(() => {
+        currentImage++;
+        updateImage();
+    }, 3000);
+}
